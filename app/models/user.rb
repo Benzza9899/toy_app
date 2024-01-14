53ee25a34c11a1cfd8767f9293ceb4c515e1f334
Benz <sipphapap9947@gmail.com>
2024-01-14 15:38:55 +0000

    Final

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates FILL_IN, presence: true # Replace FILL_IN with theright code.
-    validates FILL_IN, presence: true # Replace FILL_IN with theright code.
+    validates :microposts, presence: true
+    validates :attribute_name, presence: true
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :microposts
   resources :users
-  root 'application#hello'
+  resources :microposts
+  root 'users#index'
 
 end
